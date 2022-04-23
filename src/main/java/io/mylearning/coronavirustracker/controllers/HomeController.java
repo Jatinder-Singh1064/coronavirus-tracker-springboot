f05c7246699bd0b0838283926a033bf2f598caad
Jatinder <jatinder.1064@gmail.com>
2022-04-23 16:44:00 -0400

    First and final commit

---
+++ src/main/java/io/mylearning/coronavirustracker/controllers/HomeController.java
@@ -0,0 +1,29 @@
+package io.mylearning.coronavirustracker.controllers;
+
+import io.mylearning.coronavirustracker.models.LocationStats;
+import io.mylearning.coronavirustracker.services.CoronaVirusDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    CoronaVirusDataService coronaVirusDataService;
+
+    @GetMapping("/")
+    public String home(Model model){
+        List<LocationStats> allStats =  coronaVirusDataService.getGlobalLocStats();
+        int totalCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+        model.addAttribute("globalLocStats", allStats);
+        model.addAttribute("totalReportedCases", totalCases);
+        model.addAttribute("totalNewCases", totalNewCases);
+
+        return "home";
+    }
+}
